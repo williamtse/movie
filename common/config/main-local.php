10ce3bd7e049474544dc2e98758b738b045bda62
wiiliamtse <xwengf@126.com>
2017-04-15 14:00:21 +0000

    simpledom

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -5,7 +5,7 @@ return [
             'class' => 'yii\db\Connection',
             'dsn' => 'mysql:host=localhost;dbname=haokanxs',
             'username' => 'root',
-            'password' => '',
+            'password' => '123456',
             'charset' => 'utf8',
         ],
         'mailer' => [
